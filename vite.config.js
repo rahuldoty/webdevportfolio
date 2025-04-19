450ca1d23baa70de08872670d8733e5bd2583501
Rahul Y <>
2025-04-19 23:37:36 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/webdevportfolio/', // Update this to match your repository name
+  base: "/webdevportfolio/",
   server: {
     host: "::",
     port: 8080,
-  }
-})
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
